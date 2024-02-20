e998c4e538e6e1afdd3de0da14dac6375ae83959
Guna sekhar <131574717+gunasekhar404@users.noreply.github.com>
2024-02-20 11:26:23 +0530

    Update UserController.js

--- controller/UserController.js
+++ controller/UserController.js
@@ -70,7 +70,7 @@ module.exports.resetPassword = async (req, res) => {
 
     const sendMail = async () => {
         const info = await transporter.sendMail({
-            from: `"Gunasekhar G" <${Email_Id}>`,
+            from: `"Boobathi Thillan" <${Email_Id}>`,
             to: user.email,
             subject: "Reset Password",
             text: link
